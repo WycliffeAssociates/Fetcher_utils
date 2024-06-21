e7d8379f85e839e656ad28db46fb7800b457f6d6
Will Kelly <willkelly9617@gmail.com>
2024-06-21 16:04:57 -0500

    change run.sh again

--- run.sh
+++ run.sh
@@ -26,5 +26,6 @@ export CONTENT_URL=https://audio-content.bibleineverylanguage.org/
 export NAMESPACE_CONNECTION_STR=$(op read "op://AppDev Scripture Accessibility/languageapi-bus-con/$DEPLOY_ENV/conn_string")
 
 docker compose down
-docker compose up --build
+docker compose build
+docker compose up
 docker compose down
