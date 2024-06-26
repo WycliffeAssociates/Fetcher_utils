6f7fd59531d9e7c34199f9a8f7a4c2c33b48de15
Will Kelly <willkelly9617@gmail.com>
2024-06-26 13:16:29 -0500

    doing some debugging still

--- main.py
+++ main.py
@@ -88,8 +88,10 @@ async def read_content(content_filter: Filter):
             all_files.append(filename)
 
     # sort for debugging logs
-    all_files.sort();     
+    all_files.sort(); 
     print(f"all files on cdn len is {len(all_files)}")
+    print("all files on cdn")
+    for file in all_files: print (file)    
     for filename in all_files: 
             if "/verse/" in filename and exclude_verse:
                 continue
